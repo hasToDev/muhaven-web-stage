82f6559bb9f6894668749f03ff3ebb90e3fbf54d
hasToDev <hasdikus@gmail.com>
2026-05-08 08:35:59 +0700

    update contract

---
+++ assets/RegistryService-B0Df66nx.js
@@ -0,0 +1,1 @@
+import{dw as r,dx as d}from"./index-pMHcH3Xb.js";import{c as e}from"./provider-BeILt6EV.js";const s="InvestorRegistry",o=d.investorRegistry;async function g(t){return e(o,r,"holderCount",[t],s)}async function u(t,n,a){return e(o,r,"getHoldersPaginated",[t,n,a],s)}export{u as g,g as h};
---
+++ assets/external-DkDcJLOI.js
@@ -0,0 +1,1 @@
+const t="https://faucet.circle.com/",a="https://sepolia.arbiscan.io";function c(s){return`${a}/tx/${s}`}function n(s){return`${a}/address/${s}`}export{t as C,c as a,n as b};
