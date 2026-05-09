b6355cb0ed829c4d95c530c7c39d7bed1f807889
hasToDev <hasdikus@gmail.com>
2026-05-09 09:08:46 +0700

    update

---
+++ assets/RegistryService-CO6ACOs6.js
@@ -0,0 +1,1 @@
+import{dw as r,dx as d}from"./index-CAHrLJAA.js";import{c as e}from"./provider-Bhtm3NXT.js";const s="InvestorRegistry",o=d.investorRegistry;async function g(t){return e(o,r,"holderCount",[t],s)}async function u(t,n,a){return e(o,r,"getHoldersPaginated",[t,n,a],s)}export{u as g,g as h};
